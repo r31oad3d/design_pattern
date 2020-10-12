7a4a1d4b8d53199629a6c8f6a014cb7f55155a67
r31oad3d <r31oad3d@163.com>
2020-10-12 20:25:00 +0800

    iterator pattern

--- src/behavior_pattern/iterator_pattern/main.rs
+++ src/behavior_pattern/iterator_pattern/main.rs
@@ -1,1 +1,8 @@
-fn main() {}
+use design_pattern::behavior_pattern::iterator_pattern::{NameIterator, Iterator};
+
+fn main() {
+    let mut name_iterator = NameIterator::new(&["Robert" , "John" ,"Julie" , "Lora"]);
+    while let Some(name) = name_iterator.next() {
+        println!("name: {}", name);
+    }
+}
--- src/behavior_pattern/iterator_pattern/mod.rs
+++ src/behavior_pattern/iterator_pattern/mod.rs
@@ -1,5 +1,33 @@
 pub trait Iterator<'a> {
-    fn has_next() -> bool;
-    fn next(&mut self) -> &str;
+    fn has_next(&self) -> bool;
+    fn next(&mut self) -> Option<&str>;
 }
 
+
+pub struct NameIterator<'a> {
+    names: &'a [&'a str],
+    index: usize,
+}
+
+impl<'a> NameIterator<'a> {
+    pub fn new(names: &'a [&'a str]) -> Self {
+        NameIterator {names, index:0}
+    }
+}
+
+impl<'a> Iterator<'a> for NameIterator<'a> {
+    fn has_next(&self) -> bool {
+        self.index < self.names.len()
+    }
+
+    fn next(&mut self) -> Option<&str> {
+        if self.has_next() {
+            let temp = self.index;
+            self.index += 1;
+            Some(self.names[temp])
+        } else {
+            None
+        }
+
+    }
+}
